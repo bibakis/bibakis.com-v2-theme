9985d6faebb8ca66d7ae9f1d0060c0e32e7791bf
Vangelis Bibakis <bibakisv@gmail.com>
2020-05-13 13:11:01 +0300

    Laying the groundwork for the WordPress theme.

---
+++ WordPress/footer.php
@@ -0,0 +1,12 @@
+</div> <!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>Theme <a href="https://bibakis.com">Bibakis.com v2</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+  <?php wp_footer(); ?>
+  </body>
+</html>
