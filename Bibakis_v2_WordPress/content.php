9714cf9e8cee31287977656b2ff6a87830ab97b3
Vangelis Bibakis <bibakisv@gmail.com>
2020-07-27 23:07:41 +0300

    Headers for post page and pages. Code needs improvement.

--- Bibakis_v2_WordPress/content.php
+++ Bibakis_v2_WordPress/content.php
@@ -1,8 +1,8 @@
+<div class="blog-post-title-header">
+		<h2 class="blog-post-title"><?php echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a>';?></h2>
+</div>
 <div class="blog-post">
-	<h2 class="blog-post-title"><?php echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a>';?></h2>
-
 	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
-
- <?php the_content(); ?>
+  <?php the_content(); ?>
 
 </div><!-- /.blog-post -->
--- Bibakis_v2_WordPress/page.php
+++ Bibakis_v2_WordPress/page.php
@@ -1,17 +1,36 @@
 <?php get_header(); ?>
 
-	<div class="row">
-		<div class="col-sm-12">
 
 			<?php
 if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-	get_template_part( 'content', get_post_format() );
+echo '<div class="page-title-container">
+			<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<h2 class="page-title">' . get_the_title() . '</h2>';
+		echo '
+				</div>
+			</div>
+			</div>
+		</div>';
+
+echo '<div class="container content">';
+echo '<div class="row">
+	<div class="col-sm-12">';
+
+echo '<div class="the_page">';
+	the_content();
+
+echo '</div><!-- /.the_page -->';
+
+echo '</div> <!-- /.col-sm-12 -->
+</div> <!-- /.row -->
+</div> <!-- /.container content -->';
 
 endwhile; endif;
 			?>
 
-		</div> <!-- /.col -->
-	</div> <!-- /.row -->
+
 
 <?php get_footer(); ?>
