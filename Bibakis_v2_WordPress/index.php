73fcecda7fa56c726ca9cb750c2091e279c07bb8
Vangelis Bibakis <bibakisv@gmail.com>
2020-07-30 20:05:20 +0300

    Beautifying the code. Small CSS fixes.

--- Bibakis_v2_WordPress/index.php
+++ Bibakis_v2_WordPress/index.php
@@ -6,52 +6,53 @@ if (is_single())
 {
 	if ( have_posts() ) : while ( have_posts() ) : the_post();
 
+?>
 
-
-	echo '<div class="post-title-container">
-				<div class="container">
-				<div class="row">
-					<div class="col-sm-12">';
-						echo '<h2 class="post-title">' . get_the_title() . '</h2>';
-						echo '<p>'.the_date().'</p>';
-			echo '
+	<div class="post-title-container">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">';
+						<h2 class="post-title"><?php echo get_the_title();?></h2>
+						<p><?php echo the_date();?></p>
 					</div>
 				</div>
 				</div>
-			</div>';
+			</div>
 
-	echo '<div class="container content">';
-	echo '<div class="row">
-		<div class="col-sm-12">';
+	<div class="container content">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="the_post">
+					<?php echo the_content();?>
 
-	echo '<div class="the_post">';
-		the_content();
+				</div><!-- /.the_post -->
+			</div> <!-- /.col-sm-12 -->
+		</div> <!-- /.row -->
+	</div> <!-- /.container content -->
 
-	echo '</div><!-- /.the_post -->';
-
-	echo '</div> <!-- /.col-sm-12 -->
-	</div> <!-- /.row -->
-	</div> <!-- /.container content -->';
+<?php
 	endwhile; endif;
 }
 elseif (have_posts())
 {
-echo '<div class="container content">
-			<div class="row">
-				<div class="col-sm-12">
-					<div class="post">';
+?>
+	<div class="container content">
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="main_page_content">
 
-
+			<?php
 				if ( have_posts() ) : while ( have_posts() ) : the_post();
 	        get_template_part( 'content', get_post_format() );
 				endwhile; endif;
+			?>
 
-		echo '
+				</div>
 			</div>
 		</div>
 	</div>
-	</div>';
 
+<?php
 }
 
 ?>
--- Bibakis_v2_WordPress/page.php
+++ Bibakis_v2_WordPress/page.php
@@ -1,36 +1,31 @@
 <?php get_header(); ?>
 
-
-			<?php
+<?php
 if ( have_posts() ) : while ( have_posts() ) : the_post();
+?>
 
-echo '<div class="page-title-container">
-			<div class="container">
-			<div class="row">
-				<div class="col-sm-12">
-					<h2 class="page-title">' . get_the_title() . '</h2>';
-		echo '
-				</div>
+<div class="page-title-container">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h2 class="page-title"><?php echo get_the_title();?></h2>
 			</div>
-			</div>
-		</div>';
+		</div>
+	</div>
+</div>
 
-echo '<div class="container content">';
-echo '<div class="row">
-	<div class="col-sm-12">';
+<div class="container content">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="the_page">
+				<?php echo the_content();?>
 
-echo '<div class="the_page">';
-	the_content();
-
-echo '</div><!-- /.the_page -->';
-
-echo '</div> <!-- /.col-sm-12 -->
-</div> <!-- /.row -->
-</div> <!-- /.container content -->';
-
+			</div><!-- /.the_page -->
+		</div> <!-- /.col-sm-12 -->
+	</div> <!-- /.row -->
+</div> <!-- /.container content -->
+<?php
 endwhile; endif;
-			?>
-
-
+?>
 
 <?php get_footer(); ?>
